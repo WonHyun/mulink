4a41a3d6ab5515831501b4f0e2ddb1037f00ebc7
WonHyun <coy4728@naver.com>
2024-05-11 02:06:09 +0900

    feat: add route animation from miniplayer to musicplayer

--- lib/ui/common/mini_player/mini_player.dart
+++ lib/ui/common/mini_player/mini_player.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mulink/controller/player_controller.dart';
 import 'package:mulink/controller/playlist_controller.dart';
+import 'package:mulink/ui/common/mini_player/mini_player_controll_panel.dart';
 import 'package:mulink/ui/common/overflow_marquee.dart';
 import 'package:mulink/ui/home_screen/library_page/playlist_page/component/media_thumb_image.dart';
-import 'package:mulink/ui/music_player_screen/component/play_state_button.dart';
 import 'package:mulink/ui/music_player_screen/music_player_screen.dart';
+import 'package:animations/animations.dart';
 
 class MiniPlayer extends StatelessWidget {
   const MiniPlayer({
@@ -16,93 +16,68 @@ class MiniPlayer extends StatelessWidget {
   Widget build(BuildContext context) {
     final PlaylistController playlistController = Get.find();
 
-    return GestureDetector(
-      onTap: () => Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const MusicPlayerScreen(),
-        ),
-      ),
-      child: GetBuilder<PlaylistController>(
-        builder: (_) {
-          return Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              color: Colors.black,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Row(
-                    children: [
-                      MediaThumbImage(
-                        size: 40,
-                        albumCoverData:
-                            playlistController.currentPlayTrack?.albumCover,
-                      ),
-                      const SizedBox(width: 20),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            OverflowMarquee(
-                              text:
-                                  playlistController.currentPlayTrack?.title ??
-                                      "<unknown>",
-                            ),
-                            OverflowMarquee(
-                              text:
-                                  playlistController.currentPlayTrack?.artist ??
-                                      "<unknown>",
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const MiniPlayerControllPanel(),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class MiniPlayerControllPanel extends StatelessWidget {
-  const MiniPlayerControllPanel({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final PlayerController playerController = Get.find();
-    return GetBuilder<PlayerController>(
+    return GetBuilder<PlaylistController>(
       builder: (_) {
-        return Row(
-          children: [
-            IconButton(
-              onPressed: playerController.previous,
-              icon: const Icon(Icons.skip_previous),
-            ),
-            PlayStateButton(
-              playButtonState: playerController.playButtonState,
-              playCallback: playerController.play,
-              pauseCallback: playerController.pause,
-              buttonSize: 40,
-            ),
-            IconButton(
-              onPressed: playerController.next,
-              icon: const Icon(Icons.skip_next),
-            ),
-          ],
-        );
+        return OpenContainer(
+            closedElevation: 0,
+            closedColor: Colors.transparent,
+            middleColor: Colors.transparent,
+            openElevation: 0,
+            openColor: Colors.transparent,
+            openBuilder: (BuildContext context, VoidCallback _) {
+              return const MusicPlayerScreen();
+            },
+            transitionType: ContainerTransitionType.fade,
+            transitionDuration: const Duration(milliseconds: 500),
+            closedBuilder: (BuildContext context, VoidCallback openContainer) {
+              return Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.black,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Hero(
+                            tag: "albumCover",
+                            child: MediaThumbImage(
+                              size: 40,
+                              albumCoverData: playlistController
+                                  .currentPlayTrack?.albumCover,
+                            ),
+                          ),
+                          const SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                OverflowMarquee(
+                                  text: playlistController
+                                          .currentPlayTrack?.title ??
+                                      "<unknown>",
+                                ),
+                                OverflowMarquee(
+                                  text: playlistController
+                                          .currentPlayTrack?.artist ??
+                                      "<unknown>",
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const MiniPlayerControllPanel(),
+                  ],
+                ),
+              );
+            });
       },
     );
   }
---
+++ lib/ui/common/mini_player/mini_player_controll_panel.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mulink/controller/player_controller.dart';
+import 'package:mulink/ui/music_player_screen/component/play_state_button.dart';
+
+class MiniPlayerControllPanel extends StatelessWidget {
+  const MiniPlayerControllPanel({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final PlayerController playerController = Get.find();
+    return GetBuilder<PlayerController>(
+      builder: (_) {
+        return Row(
+          children: [
+            IconButton(
+              onPressed: playerController.previous,
+              icon: const Icon(Icons.skip_previous),
+            ),
+            PlayStateButton(
+              playButtonState: playerController.playButtonState,
+              playCallback: playerController.play,
+              pauseCallback: playerController.pause,
+              buttonSize: 40,
+            ),
+            IconButton(
+              onPressed: playerController.next,
+              icon: const Icon(Icons.skip_next),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
