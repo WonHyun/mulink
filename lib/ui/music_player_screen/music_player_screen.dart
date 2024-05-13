4ba5243d34df43944ef4e4a62f0da9ae4a676d35
WonHyun <coy4728@naver.com>
2024-05-13 23:11:58 +0900

    feat: add background color to player screen

--- lib/ui/music_player_screen/music_player_screen.dart
+++ lib/ui/music_player_screen/music_player_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mulink/controller/player_controller.dart';
 import 'package:mulink/controller/playlist_controller.dart';
+import 'package:mulink/service/util/image_util.dart';
 import 'package:mulink/ui/music_player_screen/layout/audio_progress_bar.dart';
 import 'package:mulink/ui/music_player_screen/layout/extra_controll_panel.dart';
 
@@ -18,42 +19,79 @@ class MusicPlayerScreen extends StatelessWidget {
     final PlayerController playerController = Get.find();
     final PlaylistController playlistController = Get.find();
     return PopScope(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: const Icon(Icons.keyboard_arrow_down, size: 30),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.more_horiz, size: 30),
+      child: GetBuilder<PlaylistController>(
+        builder: (_) {
+          Color trackColor = calculateAverageColor(
+              imageData: playlistController.currentPlayTrack?.albumCover);
+          return Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black, trackColor],
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: const Icon(Icons.keyboard_arrow_down, size: 30),
+                        ),
+                        IconButton(
+                          onPressed: () => {},
+                          icon: const Icon(Icons.more_vert, size: 30),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        children: [
+                          Flexible(
+                            flex: 10,
+                            child: PlayerTrackInfo(
+                              controller: playlistController,
+                            ),
+                          ),
+                          Flexible(
+                            flex: 1,
+                            child: ExtraControllPanel(
+                              controller: playerController,
+                            ),
+                          ),
+                          Flexible(
+                            flex: 1,
+                            child: AudioProgressBar(
+                              controller: playerController,
+                            ),
+                          ),
+                          Flexible(
+                            flex: 2,
+                            child: PlayerControllPanel(
+                              controller: playerController,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            children: [
-              Flexible(
-                flex: 10,
-                child: PlayerTrackInfo(controller: playlistController),
-              ),
-              Flexible(
-                flex: 1,
-                child: ExtraControllPanel(controller: playerController),
-              ),
-              Flexible(
-                flex: 1,
-                child: AudioProgressBar(controller: playerController),
-              ),
-              Flexible(
-                flex: 2,
-                child: PlayerControllPanel(controller: playerController),
-              ),
-            ],
-          ),
-        ),
+          );
+        },
       ),
     );
   }
