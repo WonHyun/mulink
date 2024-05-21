5532bf573a5f535688d30795fbae94a3ced3841a
WonHyun <coy4728@naver.com>
2024-05-21 16:43:35 +0900

    feat: register notifier by provider

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,8 +1,30 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mulink/providers/notifiers/library_notifier.dart';
+import 'package:mulink/providers/notifiers/music_player_notifier.dart';
+import 'package:mulink/providers/notifiers/queue_notifier.dart';
 import 'package:mulink/providers/states/library_state.dart';
+import 'package:mulink/providers/states/music_player_state.dart';
+import 'package:mulink/providers/states/queue_state.dart';
+import 'package:mulink/service/audio/mulink_audio_handler.dart';
+import 'package:mulink/service/service_rocator.dart';
+
+final audioHandlerProvider = Provider<JustAudioHandler>((ref) {
+  return getIt<JustAudioHandler>();
+});
 
 final libraryProvider =
     StateNotifierProvider<LibraryNotifier, LibraryState>((ref) {
-  return LibraryNotifier();
+  final queueNotifier = ref.watch(queueProvider.notifier);
+  return LibraryNotifier(queueNotifier: queueNotifier);
+});
+
+final playerProvider =
+    StateNotifierProvider<MusicPlayerNotifier, MusicPlayerState>((ref) {
+  final audioHandler = ref.watch(audioHandlerProvider);
+  return MusicPlayerNotifier(audioHandler: audioHandler);
+});
+
+final queueProvider = StateNotifierProvider<QueueNotifier, QueueState>((ref) {
+  final audioHandler = ref.watch(audioHandlerProvider);
+  return QueueNotifier(audioHandler: audioHandler);
 });
