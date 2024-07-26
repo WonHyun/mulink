a51cddd739e01bd49b9bb5488883e08538dc4c3f
WonHyun <coy4728@naver.com>
2024-07-26 18:33:33 +0900

    feat: apply flex color scheme

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:mulink/ui/home_screen/main_screen.dart';
 
@@ -7,11 +8,15 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          colorScheme: ColorScheme.dark(
-        primary: Colors.indigo.shade200,
-        secondary: Colors.indigo.shade600,
-      )),
+      themeMode: ThemeMode.dark,
+      theme: FlexThemeData.light(
+        useMaterial3: true,
+        scheme: FlexScheme.indigoM3,
+      ),
+      darkTheme: FlexThemeData.dark(
+        useMaterial3: true,
+        scheme: FlexScheme.indigoM3,
+      ),
       home: const MainScreen(),
     );
   }
